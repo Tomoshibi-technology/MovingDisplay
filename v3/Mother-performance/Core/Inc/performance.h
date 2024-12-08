fc33b57047fed152269292d62479e387d56c0d36
Neko-neko-Ne <111097589+Neko-neko-Ne@users.noreply.github.com>
2024-12-08 22:43:56 +0900

    プログラムきれいにした＆うごいた！

--- v3/Mother-performance/Core/Inc/performance.h
+++ v3/Mother-performance/Core/Inc/performance.h
@@ -16,16 +16,17 @@ class PERFORMANCE{
 	private:
 	uint16_t* perform_array;
 	int16_t* movement_array;
-	int16_t* display_array;
+	uint8_t* display_send_array;
 
 	int16_t* circle_relative_position_array;
 	int16_t* fish_relative_position_array;
 	int16_t* now_position_array;
 
 
-	int16_t circle_position_array[3];
-	int16_t fish_position_array[18];
-	int16_t line_position_array[2];
+	int16_t circle_position_array[3] = {};
+	int16_t light_status_array[9] = {};
+	int16_t fish_position_array[18] = {};
+	int16_t line_position_array[2] = {};
 
 
 	uint8_t r_standard = 10;
@@ -33,14 +34,15 @@ class PERFORMANCE{
 	uint8_t circle_H_standard = 0;
 	uint8_t S_standard = 251;
 	uint8_t V_safety = 50;
-	uint8_t V_standard = 7;
+	uint8_t V_standard = 1;
 	uint8_t V_hard = 150;
 
 
 	uint16_t shrink_const = 700;
 
 	public:
-	PERFORMANCE(uint16_t* ptr_perform_array, int16_t* ptr_display_array, int16_t* ptr_circle_relative_position_array, int16_t* ptr_fish_relative_position_array, int16_t* ptr_now_position_array);
+	PERFORMANCE(uint16_t* ptr_perform_array, uint8_t* ptr_display_send_array, int16_t* ptr_circle_relative_position_array, int16_t* ptr_fish_relative_position_array, int16_t* ptr_now_position_array);
 	void cal_drawing_status_performance(uint32_t count);
 	void get_drawing_status_performance();
+	void set_display_send_array();
 };
--- v3/Mother-performance/Core/Src/performance.cpp
+++ v3/Mother-performance/Core/Src/performance.cpp
@@ -1,9 +1,9 @@
 
 #include "performance.h"
 
-PERFORMANCE::PERFORMANCE(uint16_t* ptr_perform_array, int16_t* ptr_display_array, int16_t* ptr_circle_relative_position_array, int16_t* ptr_fish_relative_position_array, int16_t* ptr_now_position_array){
+PERFORMANCE::PERFORMANCE(uint16_t* ptr_perform_array, uint8_t* ptr_display_send_array, int16_t* ptr_circle_relative_position_array, int16_t* ptr_fish_relative_position_array, int16_t* ptr_now_position_array){
 	perform_array = ptr_perform_array;//TweLiteからのデータ
-	display_array = ptr_display_array;//displayに送るデータ
+	display_send_array = ptr_display_send_array;//displayに送るデータ
 	circle_relative_position_array = ptr_circle_relative_position_array;//円の相対座標
 	fish_relative_position_array = ptr_fish_relative_position_array;//おさかなの相対座標
 	now_position_array = ptr_now_position_array;//ロボットの絶対座標 {x, y, speed}
@@ -14,20 +14,61 @@ PERFORMANCE::PERFORMANCE(uint16_t* ptr_perform_array, int16_t* ptr_display_array
 
 }
 
-void PERFORMANCE::get_drawing_status_performance(){
+void PERFORMANCE::set_display_send_array(){
+//円
+	display_send_array[1] = circle_relative_position_array[0]+100;//左右、送信用に100たす
+	display_send_array[2] = circle_relative_position_array[1]+100;//上下、送信用に100たす
+	display_send_array[3] = circle_relative_position_array[2];//半径
 
+//光の設定
+	for(int i=0; i<9; i++){
+		display_send_array[i+4] = light_status_array[i];//光のステータスの設定
+	}
+
+//おさかなさん
+	for(int i=0; i<6; i++){
+		display_send_array[3*i+13] = fish_position_array[3*i];//モード設定
+		display_send_array[3*i+14] = fish_relative_position_array[i+2] + 100;//左右、送信用に100たす
+		display_send_array[3*i+15] = fish_position_array[3*i+2] / 10 + 100;//上下、送信用に100たす
+	}
+
+//250チェック
+	for(int i=0; i<30; i++){
+		if(display_send_array[i+1] == 250){display_send_array[i+1]++;}
+		else{}
+	}
+}
+
+void PERFORMANCE::get_drawing_status_performance(){
+//円
+	circle_relative_position_array[0] = circle_position_array[0] - now_position_array[0];//円との相対距離
+	circle_relative_position_array[0] /= 10;//cmになおす
+	circle_relative_position_array[0] += 24;//原点を左下にした値にする
+
+	circle_relative_position_array[1] = circle_position_array[1] / 10;//上下方向、cmになおす
+	circle_relative_position_array[2] = circle_position_array[2] / 10;//半径、cmになおす
+
+//おさかなさん
+	for(int i=0; i<6; i++){
+		fish_relative_position_array[3*i] = fish_position_array[3*i];//モード
+
+		fish_relative_position_array[3*i+1] = fish_position_array[3*i+1] - now_position_array[0];//さかなとの相対距離
+		fish_relative_position_array[3*i+1] /= 10;//cmになおす
+		fish_relative_position_array[3*i+1] += 24;//原点を左下にした値にする
+
+		fish_relative_position_array[3*i] = fish_position_array[3*i+2] / 10;//上下方向、cmになおす
+	}
 }
 
 
-void PERFORMANCE::cal_drawing_status_perfomance(uint32_t count){
+void PERFORMANCE::cal_drawing_status_performance(uint32_t count){
 
 //	perform_array[0] = hue
 //	perform_array[1] = θ
 //	perform_array[2] = r
 //	perform_array[3] = mode
 
-	//送られてきた速度ベクトルを処理して、目標速度ベクトルを決める
-	//モードを処理して描画モードを選択する
+	//描画モードを選択する
 	//停止、まる固定、おさかな固定,まる運び
 
 
@@ -56,64 +97,65 @@ void PERFORMANCE::cal_drawing_status_perfomance(uint32_t count){
 		}else{}
 	}else{}
 
-//まるとかの光
+
+//光の設定
 	if(perform_array[3] == 0){
-		display_array[4] = perform_array[0] + 128;//circle_H
-		display_array[5] = S_standard;//circle_S
-		display_array[6] = V_standard;//circle_V
-		display_array[7] = perform_array[0];//background_H
-		display_array[8] = S_standard;//background_S
-		display_array[9] = V_standard;//background_V
-		display_array[10] = perform_array[0];//frame_H
-		display_array[11] = S_standard;//frame_S
-		display_array[12] = V_standard;//frame_V
+		light_status_array[0] = perform_array[0] + 128;//circle_H
+		light_status_array[1] = S_standard;//circle_S
+		light_status_array[2] = V_standard;//circle_V
+		light_status_array[3] = perform_array[0];//background_H
+		light_status_array[4] = S_standard;//background_S
+		light_status_array[5] = V_standard;//background_V
+		light_status_array[6] = perform_array[0];//frame_H
+		light_status_array[7] = S_standard;//frame_S
+		light_status_array[8] = V_standard;//frame_V
 
 	}else if(perform_array[3] == 1){
-		display_array[4] = perform_array[0] + 128;//circle_H
-			display_array[4] %= 256;
-		display_array[5] = S_standard;//circle_S
-		display_array[6] = V_standard;//circle_V
-		display_array[7] = perform_array[0];//background_H
-		display_array[8] = S_standard;//background_S
-		display_array[9] = V_standard;//background_V
-		display_array[10] = perform_array[0];//frame_H
-		display_array[11] = S_standard;//frame_S
-		display_array[12] = V_standard;//frame_V
+		light_status_array[0] = perform_array[0] + 128;//circle_H
+			light_status_array[0] %= 256;
+		light_status_array[1] = S_standard;//circle_S
+		light_status_array[2] = V_standard;//circle_V
+		light_status_array[3] = perform_array[0];//background_H
+		light_status_array[4] = S_standard;//background_S
+		light_status_array[5] = V_standard;//background_V
+		light_status_array[6] = perform_array[0];//frame_H
+		light_status_array[7] = S_standard;//frame_S
+		light_status_array[8] = V_standard;//frame_V
 
 	}else if(perform_array[3] == 2){
-		display_array[4] = perform_array[0];//circle_H
-		display_array[5] = S_standard;//circle_S
-		display_array[6] = V_standard;//circle_V
-		display_array[7] = perform_array[0];//background_H
-		display_array[8] = S_standard;//background_S
-		display_array[9] = V_standard;//background_V
-		display_array[10] = perform_array[0];//frame_H
-		display_array[11] = S_standard;//frame_S
-		display_array[12] = V_standard;//frame_V
+		light_status_array[0] = perform_array[0];//circle_H
+		light_status_array[1] = S_standard;//circle_S
+		light_status_array[2] = V_standard;//circle_V
+		light_status_array[3] = perform_array[0];//background_H
+		light_status_array[4] = S_standard;//background_S
+		light_status_array[5] = V_standard;//background_V
+		light_status_array[6] = perform_array[0];//frame_H
+		light_status_array[7] = S_standard;//frame_S
+		light_status_array[8] = V_standard;//frame_V
 
 	}else if(perform_array[3] == 3){
-		display_array[4] = perform_array[0] + 128;//circle_H
-			display_array[4] %= 256;
-		display_array[5] = S_standard;//circle_S
-		display_array[6] = V_standard;//circle_V
-		display_array[7] = perform_array[0];//background_H
-		display_array[8] = S_standard;//background_S
-		display_array[9] = V_standard;//background_V
-		display_array[10] = perform_array[0] + 128;//枠表示
-			display_array[10] %= 256;
-		display_array[11] = S_standard;//frame_S
-		display_array[12] = V_standard;//frame_V
+		light_status_array[0] = perform_array[0] + 128;//circle_H
+			light_status_array[0] %= 256;
+		light_status_array[1] = S_standard;//circle_S
+		light_status_array[2] = V_standard;//circle_V
+		light_status_array[3] = perform_array[0];//background_H
+		light_status_array[4] = S_standard;//background_S
+		light_status_array[5] = V_standard;//background_V
+		light_status_array[6] = perform_array[0] + 128;//枠表示
+			light_status_array[6] %= 256;
+		light_status_array[7] = S_standard;//frame_S
+		light_status_array[8] = V_standard;//frame_V
 
 	}else{
-		display_array[4] = perform_array[0] + 128;//circle_H
-		display_array[5] = S_standard;//circle_S
-		display_array[6] = V_standard;//circle_V
-		display_array[7] = perform_array[0];//background_H
-		display_array[8] = S_standard;//background_S
-		display_array[9] = V_standard;//background_V
-		display_array[10] = perform_array[0];//frame_H
-		display_array[11] = S_standard;//frame_S
-		display_array[12] = V_standard;//frame_V
+		light_status_array[0] = perform_array[0] + 128;//circle_H
+		light_status_array[1] = S_standard;//circle_S
+		light_status_array[2] = V_standard;//circle_V
+		light_status_array[3] = perform_array[0];//background_H
+		light_status_array[4] = S_standard;//background_S
+		light_status_array[5] = V_standard;//background_V
+		light_status_array[6] = perform_array[0];//frame_H
+		light_status_array[7] = S_standard;//frame_S
+		light_status_array[8] = V_standard;//frame_V
 	}
 
 
@@ -121,52 +163,52 @@ void PERFORMANCE::cal_drawing_status_perfomance(uint32_t count){
 	if(perform_array[3] == 2){
 		fish_position_array[0] = 2;//ID
 		fish_position_array[1] = circle_position_array[0];//X
-		fish_position_array[2] = 150;//Y
+		fish_position_array[2] = 150;//Z
 
 		fish_position_array[3] = 2;//ID
 		fish_position_array[4] = circle_position_array[0] + 400;//X
-		fish_position_array[5] = 300;//Y
+		fish_position_array[5] = 300;//Z
 
 		fish_position_array[6] = 2;//ID
 		fish_position_array[6] = circle_position_array[0] - 200;//X
-		fish_position_array[8] = 50;//Y
+		fish_position_array[8] = 50;//Z
 
 		fish_position_array[9] = 2;//ID
 		fish_position_array[10] = circle_position_array[0] + 700;//X
-		fish_position_array[11] = 75;//Y
+		fish_position_array[11] = 75;//Z
 
 		fish_position_array[12] = 2;//ID
 		fish_position_array[13] = circle_position_array[0] - 400;//X
-		fish_position_array[14] = 250;//Y
+		fish_position_array[14] = 250;//Z
 
 		fish_position_array[15] = 2;//ID
 		fish_position_array[16] = circle_position_array[0] - 700;//X
-		fish_position_array[17] = 200;//Y
+		fish_position_array[17] = 200;//Z
 
 	}else{
 		fish_position_array[0] = 0;//ID
 		fish_position_array[1] = 1500;//X
-		fish_position_array[2] = 150;//Y
+		fish_position_array[2] = 150;//Z
 
 		fish_position_array[3] = 0;//ID
 		fish_position_array[4] = 1900;//X
-		fish_position_array[5] = 300;//Y
+		fish_position_array[5] = 300;//Z
 
 		fish_position_array[6] = 0;//ID
 		fish_position_array[7] = 2300;//X
-		fish_position_array[8] = 150;//Y
+		fish_position_array[8] = 150;//Z
 
 		fish_position_array[9] = 0;//ID
 		fish_position_array[10] = 2700;//X
-		fish_position_array[11] = 300;//Y
+		fish_position_array[11] = 300;//Z
 
 		fish_position_array[12] = 0;//ID
 		fish_position_array[13] = 3100;//X
-		fish_position_array[14] = 150;//Y
+		fish_position_array[14] = 150;//Z
 
 		fish_position_array[15] = 0;//ID
 		fish_position_array[16] = 3500;//X
-		fish_position_array[17] = 300;//Y
+		fish_position_array[17] = 300;//Z
 	}
 
 
