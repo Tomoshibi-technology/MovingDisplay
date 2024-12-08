0740d3a689e4490999991dd1c82d6b58126be33d
Jun-robot <saijun721.1@gmail.com>
2024-12-08 19:46:39 +0900

    add main.py

---
+++ v3/pc-gui/main.py
@@ -0,0 +1,26 @@
+import serial
+import struct
+
+def send( buf ):
+      while True:
+            if ser.out_waiting == 0:
+                  break
+      for b in buf:
+            a = struct.pack( "B", b )
+            ser.write(a)
+      ser.flush()
+
+ser = serial.Serial(
+      port = "/dev/cu.wchusbserial110",
+      baudrate = 115200,
+      parity = serial.PARITY_NONE,
+      bytesize = serial.EIGHTBITS,
+      stopbits = serial.STOPBITS_ONE,
+      #timeout = None,
+      #xonxoff = 0,
+      #rtscts = 0,
+      )
+
+while True:
+    x = [ 0xFA, 0xFF, 0x02, 0xFE, 0x03]
+    send( x )
